4ec32c83398be67b3789522dd2dcb924d86bc639
tayfunai <asliddinmalikov999@gmail.com>
2024-02-26 09:00:21 +0500

    project is ready to deploy

--- .ipynb_checkpoints/penguins-app-checkpoint.py
+++ .ipynb_checkpoints/penguins-app-checkpoint.py
@@ -1,5 +1,3 @@
-import pickle
-
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -24,7 +22,7 @@ if uploaded_file is not None:
     input_df = pd.read_csv(uploaded_file)
 else:
     def user_input_features():
-        island = st.sidebar.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
+        island =  st.sidebar.selectbox('Island', ('Bisco', 'Dream', 'Torgerson'))
         sex = st.sidebar.selectbox('Sex', ('male', 'female'))
         bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1, 59.6, 43.9)
         bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
@@ -36,49 +34,9 @@ else:
             'bill_length_mm': bill_length_mm,
             'bill_depth_mm': bill_depth_mm,
             'flipper_length_mm': flipper_length_mm,
-            'body_mass_g': body_mass_g,
-            'sex': sex
+            'body_mass_g': body_mass_g
         }
         features = pd.DataFrame(data, index=[0])
         return features
 
-
     input_df = user_input_features()
-
-penguins_raw = pd.read_csv('./dataset/penguins_cleaned.csv')
-penguins = penguins_raw.drop(columns=['species'])
-
-df = pd.concat([input_df, penguins], axis=0)
-
-encode = ['sex', 'island']
-for col in encode:
-    dummy = pd.get_dummies(df[col], prefix=col)
-    df = pd.concat([df, dummy], axis=1)
-    del df[col]
-df = df[:1]
-
-st.subheader('User Input Features')
-
-if uploaded_file is not None:
-    st.write(df)
-else:
-    st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
-    st.write(df)
-
-# Reads in a saved classification model
-
-load_clf = pickle.load(open(
-    'penguins_clf.pkl', 'rb'
-))
-
-# Apply  model to make predictions
-prediction = load_clf.predict(df)
-prediction_proba = load_clf.predict_proba(df)
-
-
-st.subheader('Prediction')
-penguins_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
-st.write(penguins_species[prediction])
-
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
